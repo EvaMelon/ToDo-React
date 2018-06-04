69f33c51fdf2119aad877f83ff4c092951904351
EvaMelon <ewamelon08@gmail.com>
2018-06-04 14:36:19 +1000

    added buttonColorChange

--- src/App.js
+++ src/App.js
@@ -1,32 +1,79 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import './reset.css';
 import './App.css';
-import './oop';
+import './oop'; 
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      items:[{text:"g", done:false}, {text:"u", done:false}]
+    };
+  }
+  // Tak deklarujemy metody, ktore chcemy przekazac innym komponentom np. w onKeyPress etc.
+  handleKeyPress = (event) => { //event listener onkeypress
+    const enterKey = 13;
+    if (event.which == enterKey) {
+      const items = this.state.items;
+      const newItem = {text:event.target.value, done:false};
+      const newItems = items.concat(newItem); //zwraca tablice, ktora jest sklejeniem items i argumentow, ktore przekazesz
+      this.setState({
+        items: newItems
+      });
+      event.target.value="";
+    }
+  }
+  renderItems() {
+    const items = [];
+    for(let i=0; i<this.state.items.length; i++) {
+        let itemArray = <TodoItem text={this.state.items[i].text} />;
+        items.push(itemArray);
+    }
+    return items;
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className="App-title">todos</h1>
-        <button type="button" class="button">v</button>
-        <input type="text" class="todos-writingSpace" placeholder="What needs to be done?"/>
-        <ul>
-          <li class="itemList">jfd</li>
-        </ul>
-      
+        <div className="fieldSet">
+          <button type="button" className="button" 
+          onClick={this.buttonColorChange()}>v</button>
+          <input type="text" className="todos-writingSpace" placeholder="What needs to be done?"
+                 onKeyPress={this.handleKeyPress}/>
+        </div>
+        <div className="items-section">
+          {this.renderItems()}
+        </div>
       </div>
     );
     
   }
 }
 
-function adder() {
-  let button = document.querySelector("button");
-  let singleItem = document.querySelector("itemList");
-
+buttonColorChange = (event) => {
+  const button = this.button;
+  button.onClick.classList.add("blackButton");
 }
 
-//jezeli ktos kliknie na button, to zmienia sie visibility singleitem i 
-//dodaje sie to co jest w input text
+class TodoItem extends React.Component {
+ render() {
+   return (
+     <div className="todo">{this.props.text}</div>
+   );
+
+ }
+}
+
+
 
 export default App;
+
+//znalzec przycisk i zmienic mu kolor na czarny po kliknieciu, bedzie mial dodawana klase z kolorem
+
+
+
+
+
